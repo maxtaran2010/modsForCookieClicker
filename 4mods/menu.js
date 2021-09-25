27c3cb2ddd7814f9d747aac694988a14a57e4e17
maxtaran2010 <max.taran.210@mail.ru>
2021-09-25 14:45:48 +0300

    added userscript

--- 4mods/menu.js
+++ 4mods/menu.js
@@ -1,3 +1,16 @@
+// ==UserScript==
+// @name         ''Script Name''
+// @namespace    ''Github Page''
+// @homepageURL  ''Script.js''
+// @updateURL    ''Script.js''
+// @downloadURL  ''Script.js''
+// @version      ''Current Version'' (Not Required)
+// @description  Load the Script when Cookie Clicker starts
+// @author       ''Author Name''
+// @match        https://orteil.dashnet.org/cookieclicker/
+// @grant        none
+// ==/UserScript==
+
 document.domain = "dashnet.org";
 alert('you are not AFK?');
 alert('game is started');
