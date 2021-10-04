3fefa4ad148698576ef81d914ba40f618cdfcc66
maxtaran2010 <max.taran.210@mail.ru>
2021-10-04 09:23:54 +0300

    4mods

--- 4mods/4mods.js
+++ 4mods/4mods.js
@@ -1,1 +1,5 @@
-alert("hello!")
+var ForMods = {};
+
+ForMods.start = function() {
+    alert("4mods loaded. Press the '4mods' button in top bar to open menu.")
+}
--- 4mods/4modsLoader.js
+++ 4mods/4modsLoader.js
@@ -10,5 +10,30 @@
 // @match        https://orteil.dashnet.org/cookieclicker/
 // @grant        none
 // ==/UserScript==
-console.log("~hello form 4mods!")
-Game.loadMod("https://raw.githubusercontent.com/maxtaran2010/modsForCookieClicker/master/4mods/4mods.js")
+window.eval("(" + (function() {
+    'use strict';
+
+    var loadFunc = function() {
+        var Game = window.Game;
+        if(Game && Game.ready) {
+            Game.loadMod("https://raw.githubusercontent.com/maxtaran2010/modsForCookieClicker/master/4mods/4mods.js")
+            console.log("~hello form 4mods!")
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if(window.AcharvaksAgronomicon === undefined) {
+        window.AcharvaksAgronomicon = { preloader: true };
+    }
+    if(!window.AcharvaksAgronomicon.loaded) {
+        if(!loadFunc()) {
+            var interval = setInterval(function() {
+                if(loadFunc()) {
+                    clearInterval(interval);
+                }
+            }, 1000);
+        }
+    }
+}).toString() + ")()");
